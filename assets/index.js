f6db9e630da5e117916a96117a640fccbf25287c
eatontechnology <eaton.technology@gmail.com>
2024-02-15 15:14:55 +0000

    added pseudo code

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+//I want users to only be able to add valid names
+//I want users to only be able to add valid email addresses
+//I want users to only be able to add valid credit/debit card information
+//alll validaiton to be done on one page utilisiing local.storage
+//issues being highlighted to the user when they are still entering a field of as soon as possible after
+//correct fields should be shown as DN green, errors shown in DN pink
+//ensure that any data capture is secure and there is no risk of database corruption through SQL injection. Only standard upper / lower case. letters and printable characters: !#$%&'*+-/=?^_`{|}~ should be allowed in all fields.
+//ensure that all accepted credit cards conform to the LUHN algorithm.
+//make sure mobile compatible (see CSS)
+//I need an Email to test@dn-uk.com be sent containing the validated information when a user presses a ‘Submit’ button.
